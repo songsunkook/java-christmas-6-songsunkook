674fdc2c8017908c232aa100b64f57842b2736d1
songsunkook <songsunkook@gmail.com>
2023-11-13 21:28:08 +0900

    feat: 특정 금액 이상 구매 시 증정품을 준다.

---
+++ src/main/java/domain/giveaway/Giveaway.java
@@ -0,0 +1,23 @@
+package domain.giveaway;
+
+import constant.Drink;
+import constant.Menu;
+
+public class Giveaway {
+    private static final Menu GIVEAWAY_PRIZE = Drink.CHAMPAGNE;
+    private static final int DISCOUNT_STANDARD_PRICE = 120_000;
+    
+    private final boolean haveGiveaway;
+    
+    public Giveaway(int amountBeforeDiscount) {
+        haveGiveaway = amountBeforeDiscount >= DISCOUNT_STANDARD_PRICE;
+    }
+    
+    public boolean isHave() {
+        return haveGiveaway;
+    }
+    
+    public Menu getGivewayPrize() {
+        return GIVEAWAY_PRIZE;
+    }
+}
---
+++ src/test/java/domain/giveaway/GiveawayTest.java
@@ -0,0 +1,37 @@
+package domain.giveaway;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import constant.Drink;
+import org.junit.jupiter.api.Test;
+
+class GiveawayTest {
+    
+    @Test
+    public void 특정_금액_이상_주문_시_증정품이_주어지는가() {
+        //given
+        Giveaway noGiveaway1 = new Giveaway(10000);
+        Giveaway noGiveaway2 = new Giveaway(50000);
+        Giveaway noGiveaway3 = new Giveaway(100000);
+        Giveaway giveaway1 = new Giveaway(120000);
+        Giveaway giveaway2 = new Giveaway(200000);
+        
+        //then
+        assertFalse(noGiveaway1.isHave());
+        assertFalse(noGiveaway2.isHave());
+        assertFalse(noGiveaway3.isHave());
+        assertTrue(giveaway1.isHave());
+        assertTrue(giveaway2.isHave());
+    }
+    
+    @Test
+    public void 상품이_올바르게_증정되는가() {
+        //given
+        Giveaway giveaway1 = new Giveaway(120000);
+        Giveaway giveaway2 = new Giveaway(200000);
+        
+        //then
+        assertEquals(Drink.CHAMPAGNE, giveaway1.getGivewayPrize());
+        assertEquals(Drink.CHAMPAGNE, giveaway2.getGivewayPrize());
+    }
+}
